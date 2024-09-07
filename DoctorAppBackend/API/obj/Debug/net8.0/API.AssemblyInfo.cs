1b5561c8a5daa0d53806ff240d9bfa2b48a70399
GilABst <gil.bastien@outlook.com>
2024-09-07 14:32:17 -0600

    Login Complete

--- DoctorAppBackend/API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ DoctorAppBackend/API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3fa02255caa9d9d9da9f22fc9a06a7ee9e8f71b4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b748a325ff0035d839b0e0daeb590da765b36896")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
